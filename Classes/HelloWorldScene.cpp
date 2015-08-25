fb6b94f858b08de461734a56ae0d1effc0ce2c22
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-25 23:55:42 +0300

    Remove a label showing "Hello World"

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -48,21 +48,6 @@ bool HelloWorld::init()
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    
-    // position the label on the center of the screen
-    label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
-
-    // add the label as a child to this layer
-    this->addChild(label, 1);
-
     return true;
 }
 
