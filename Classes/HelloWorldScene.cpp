701ca402cf239a44455df187f433141b56c1b855
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-08-26 00:15:27 +0300

    Various fixes

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -20,15 +20,8 @@ Scene* HelloWorld::createScene()
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-	//////////////////////////////
-	// 1. super init first
-	if ( !Layer::init() )
-	{
+	if (!Layer::init())
 		return false;
-	}
 	
-	Size visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
 	return true;
 }
